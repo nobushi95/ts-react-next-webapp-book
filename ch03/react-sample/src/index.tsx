ca125fa1e46072f984d7e6d51d1c1f22c7bc8854
nobushi95 <nobushi1121@gmail.com>
2023-12-03 18:22:15 +0900

    Helloコンポーネント追加

--- ch03/react-sample/src/index.tsx
+++ ch03/react-sample/src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Hello from './components/Hello';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
@@ -9,7 +9,8 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Hello />
   </React.StrictMode>
 );
 
