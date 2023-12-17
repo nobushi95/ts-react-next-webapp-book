9747b992a31db56358791991a1d91fe24d99b714
nobushi95 <nobushi1121@gmail.com>
2023-12-17 15:07:41 +0900

    ParentMemoComponentの追加

--- ch03/react-sample/src/index.tsx
+++ ch03/react-sample/src/index.tsx
@@ -8,6 +8,7 @@ import { CounterUseState } from './components/CounterUseState';
 import { Hello } from './components/Hello';
 import { Message } from './components/Message';
 import { Name } from './components/Name';
+import { ParentMemoComponent } from './components/ParentMemoComponent';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
@@ -24,6 +25,7 @@ root.render(
     <ContextSample />
     <CounterUseState initialValue={0} />
     <CounterUseReducer initialValue={0} />
+    <ParentMemoComponent />
   </React.StrictMode>
 );
 
