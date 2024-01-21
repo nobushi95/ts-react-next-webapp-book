2cade988858dbf98b604624740802671c5957434
nobushi95 <nobushi1121@gmail.com>
2024-01-21 14:30:17 +0900

    styled-componentsをindex.tsxに適用

--- ch04/component-development/pages/index.tsx
+++ ch04/component-development/pages/index.tsx
@@ -1,7 +1,12 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import Image from 'next/image';
+import styled from 'styled-components';
+import styles from '../styles/Home.module.css';
+
+const H1 = styled.h1`
+  color: red;
+`;
 
 const Home: NextPage = () => {
   return (
@@ -13,9 +18,9 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
+        <H1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        </H1>
 
         <p className={styles.description}>
           Get started by editing{' '}
@@ -66,7 +71,7 @@ const Home: NextPage = () => {
         </a>
       </footer>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
